5d80caa46fe0f3cd9f26a73569d25cf27dcd5bf2
AkshataAnnigeri11 <akshata.annigeri@pronto-ss.com>
2024-06-11 12:24:46 +0530

    Delete Developement/Java directory

--- Developement/Java/Employee-Managenment/src/main/java/com/java/employeemanagement/repository/EmployeeDesignationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.employeemanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java.employeemanagement.model.EmployeeDesignation;
-
-public interface EmployeeDesignationRepository extends JpaRepository<EmployeeDesignation, Integer> {
-
-}
